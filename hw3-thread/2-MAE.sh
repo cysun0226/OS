0867bfc833db0f6ba9d0f8507c614460ab4bf5c8
cysun0226 <j6256448@gmail.com>
2017-12-04 02:25:05 +0800

    Add 2-MAE.sh

---
+++ hw3-thread/2-MAE.sh
@@ -0,0 +1,10 @@
+echo "--- check Sobel1.bmp ---"
+./MAE.out ANS/Sobel1.bmp Sobel1.bmp
+echo "--- check Sobel2.bmp ---"
+./MAE.out ANS/Sobel2.bmp Sobel2.bmp
+echo "--- check Sobel3.bmp ---"
+./MAE.out ANS/Sobel3.bmp Sobel3.bmp
+echo "--- check Sobel4.bmp ---"
+./MAE.out ANS/Sobel4.bmp Sobel4.bmp
+echo "--- check Sobel5.bmp ---"
+./MAE.out ANS/Sobel5.bmp Sobel5.bmp
