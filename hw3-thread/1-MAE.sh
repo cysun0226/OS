48a8456eefcb8039e20f70fcdf9e6d4bb144696e
cysun0226 <j6256448@gmail.com>
2017-12-04 02:21:31 +0800

    Add 1-MAE.sh

---
+++ hw3-thread/1-MAE.sh
@@ -0,0 +1,11 @@
+echo "--- check Blur1.bmp ---"
+./MAE.out ANS/Blur1.bmp Blur1.bmp
+echo "--- check Blur2.bmp ---"
+./MAE.out ANS/Blur2.bmp Blur2.bmp
+echo "--- check Blur3.bmp ---"
+./MAE.out ANS/Blur3.bmp Blur3.bmp
+echo "--- check Blur4.bmp ---"
+./MAE.out ANS/Blur4.bmp Blur4.bmp
+echo "--- check Blur5.bmp ---"
+./MAE.out ANS/Blur5.bmp Blur5.bmp
+
