453d7cf114bfc5a577bc3e72b6e29f100cab987b
cysun0226 <j6256448@gmail.com>
2017-12-05 00:31:48 +0800

    Update 2-MAE.sh

--- hw3-thread/2-MAE.sh
+++ hw3-thread/2-MAE.sh
@@ -1,4 +1,6 @@
-echo "--- check Sobel1.bmp ---"
+g++ -std=c++11 -pthread $1 -o sobel.out
+./sobel.out
+echo "\n--- check Sobel1.bmp ---"
 ./MAE.out ANS/Sobel1.bmp Sobel1.bmp
 echo "--- check Sobel2.bmp ---"
 ./MAE.out ANS/Sobel2.bmp Sobel2.bmp
@@ -8,3 +10,5 @@ echo "--- check Sobel4.bmp ---"
 ./MAE.out ANS/Sobel4.bmp Sobel4.bmp
 echo "--- check Sobel5.bmp ---"
 ./MAE.out ANS/Sobel5.bmp Sobel5.bmp
+
+./2-clean.sh
