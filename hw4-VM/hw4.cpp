aadf65d0f0a24af20033fdb4cfde5b561728b109
cysun0226 <j6256448@gmail.com>
2018-01-02 14:07:17 +0800

    create hw4 folder

---
+++ hw4-VM/hw4.cpp
@@ -0,0 +1,17 @@
+// Student ID : 0416045
+// Name       : cysun
+// Date       : 2018.01.02
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+#define
+
+int main(int argc, char* argv[])
+{
+	fstream backing_file, addr_file;
+	backing_file.open(argv[1], ios::in);
+	addr_file.open(argv[2], ios::in);
+
+}
