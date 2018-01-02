eda6c7c70732bd662605b35d0670b9f1a38b9f6b
cysun0226 <j6256448@gmail.com>
2018-01-02 14:31:43 +0800

    reading file, size define

--- hw4-VM/hw4.cpp
+++ hw4-VM/hw4.cpp
@@ -6,7 +6,12 @@
 #include <fstream>
 using namespace std;
 
-#define
+#define TLB_SIZE 16
+#define PAGE_TABLE_SIZE 256
+#define PAGE_SIZE 256
+#define FRAME_SIZE 256
+#define FRAME_NUM 256
+#define PHYSICAL_MEMORY 65536
 
 int main(int argc, char* argv[])
 {
@@ -14,4 +19,9 @@ int main(int argc, char* argv[])
 	backing_file.open(argv[1], ios::in);
 	addr_file.open(argv[2], ios::in);
 
+	unsigned short log_addr;
+	for (size_t i = 0; i < 10; i++) {
+		addr_file >> log_addr;
+		cout << log_addr << endl;
+	}
 }
